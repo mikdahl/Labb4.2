5a2a8487d5eb7bd09870113c49bc45b4a62c606d
Oskar S <Oskar.filip.sjoberg@gmail.com>
2021-11-16 14:36:47 +0100

    General changes and fixes on mobile menu

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var mobileNav = document.getElementById("mobileNav");
 var result =""; //Variable for storing the result of the HTML & Array output
+var headerNav = document.getElementById("nav-item1");
+//var closeIcon ="closeIcon.png";
+//var navIcon;
 
 var linkArray = ["News", "About", "Press", "Careers", "Contact"];
 
@@ -10,14 +13,16 @@ for(var i = 0; i < linkArray.length; i ++ ){
 }
 
 
-
-
 displayLinks();
 
 function displayLinks(){
     if(mobileNav.style.display ==="none"){
     mobileNav.style.display ="block";
-    } else{
+    mobileNav.style.boxShadow ="0px 2px 2px rgb(0, 0, 0, 0.10)";
+        headerNav.style.boxShadow = "none";
+
+    } else {
         mobileNav.style.display="none";
+        headerNav.style.boxShadow = "0px 4px 4px rgb(0, 0, 0, 0.25)";
     }
 }
